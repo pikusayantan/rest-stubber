55cadcc07b2ad0d68f3e65bf862bcb7ecaeec810
pikusayantan <pikusayantan@gmail.com>
2018-07-31 04:32:49 +0530

    initial commit

---
+++ src/main/java/com/vidhilekh/stubber/rest/controller/RestRequestController.java
@@ -0,0 +1,37 @@
+package com.vidhilekh.stubber.rest.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class RestRequestController {
+	
+	@RequestMapping(path = "/**", consumes = MediaType.ALL_VALUE , produces = MediaType.ALL_VALUE)
+	public ResponseEntity<String> allRequest(HttpServletRequest httpRequest, @RequestBody(required = false) String reqBody) throws JsonParseException, JsonMappingException, IOException{
+		System.out.println("resource url:" + httpRequest.getRequestURI()+" Method:"+httpRequest.getMethod());
+		ObjectMapper objectMapper = new ObjectMapper();
+	    JsonNode jsonNode = objectMapper.readValue(reqBody, JsonNode.class);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("content-type", MediaType.APPLICATION_JSON_VALUE).body(jsonNode.toString());
+	}
+	
+	@RequestMapping(path = "/**", method=RequestMethod.GET, consumes = MediaType.ALL_VALUE , produces = MediaType.ALL_VALUE)
+	public ResponseEntity<String> getRequest(HttpServletRequest httpRequest) throws JsonParseException, JsonMappingException, IOException{
+		System.out.println("resource url:" + httpRequest.getRequestURI()+" Method:"+httpRequest.getMethod());
+		return ResponseEntity.status(HttpStatus.OK).header("content-type", MediaType.APPLICATION_JSON_VALUE).body("{\"one\": \"AAA\"}");
+	}
+
+}
