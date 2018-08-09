6cd8b1dd5c22e8780112b655eff737965898d47d
pikusayantan <pikusayantan@gmail.com>
2018-08-10 01:42:58 +0530

    added model class and initial draft of first page with css

---
+++ src/main/java/com/vidhilekh/stubber/rest/model/registration/LoggedUser.java
@@ -0,0 +1,15 @@
+package com.vidhilekh.stubber.rest.model.registration;
+
+public class LoggedUser {
+	
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
